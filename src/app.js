0820fb45fa1e5246eb5ca8e0bd152a621f92023c
FelipeReis1 <feliperg15@yahoo.com.br>
2023-01-20 19:50:16 -0300

    sign-up concluded

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express, { json } from "express";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 import chalk from "chalk";
+import joi from "joi";
+import bcrypt from "bcrypt";
 
 const PORT = 5000;
 const app = express();
@@ -19,6 +21,40 @@ try {
 } catch (err) {
   console.log(`Something went wrong ${err.message}`);
 }
+
+app.post("/sign-up", async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+
+  const signUpSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required(),
+    password: joi.string().required(),
+    confirmPassword: joi.string().required(),
+  });
+  const signUpValidation = signUpSchema.validate(
+    { name, email, password, confirmPassword },
+    { abortEarly: false }
+  );
+  if (signUpValidation.error) {
+    const errors = signUpValidation.details.map((r) => r.message);
+    return res.status(422).send(errors);
+  }
+
+  try {
+    const user = await db.collection("users").findOne({ email });
+    if (user) {
+      return res.status(409).send("Usuário já registrado!");
+    }
+    const encryptedPassword = bcrypt.hashSync(password, 10);
+    await db.collection("users").insertOne({
+      name,
+      email,
+      password: encryptedPassword,
+      confirmPassword: encryptedPassword,
+    });
+    res.status(201).send("Usuário registrado com sucesso!");
+  } catch (error) {}
+});
 
 app.listen(PORT, () =>
   console.log(chalk.bold.blue(`Server running on port: ${PORT}`))
